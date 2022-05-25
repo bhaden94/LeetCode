0b58f5aa713fee7c1a9fc662a70924e9b39b1940
Brady Haden <59949412+bhaden94@users.noreply.github.com>
2022-05-25 10:48:50 -0400

    Time: 2 ms, Memory: 41.9 MB - LeetHub

--- symmetric-tree/symmetric-tree.java
+++ symmetric-tree/symmetric-tree.java
@@ -13,18 +13,54 @@
  *     }
  * }
  */
-class Solution {
-    private boolean helper(TreeNode left, TreeNode right) {
-        if (left == null && right == null) return true;
-        if (left != null && right != null && left.val == right.val) {
-            return (helper(left.left, right.right) &&
-                    helper(left.right, right.left));
-        }
-        return false;
-    }
+
+// Recursion
+// class Solution {
+//     private boolean helper(TreeNode left, TreeNode right) {
+//         if (left == null && right == null) return true;
+//         if (left != null && right != null && left.val == right.val) {
+//             return (helper(left.left, right.right) &&
+//                     helper(left.right, right.left));
+//         }
+//         return false;
+//     }
     
+//     public boolean isSymmetric(TreeNode root) {
+//         if (root == null) return false;
+//         return helper(root, root);
+//     }
+// }
+
+// Iterative
+class Solution {
     public boolean isSymmetric(TreeNode root) {
         if (root == null) return false;
-        return helper(root, root);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root.left);
+        q.add(root.right);
+        
+        while (!q.isEmpty()) {
+            TreeNode left = q.remove();
+            TreeNode right = q.remove();
+            
+            if (left == null && right == null) continue;
+            
+            // if one null
+            if (left == null && right != null ||
+               left != null && right == null) {
+                return false;
+            }
+                
+            // if don't equal
+            if (left.val != right.val) return false;
+            
+            // add to queue
+            q.add(left.left);
+            q.add(right.right);
+            q.add(left.right);
+            q.add(right.left);
+        }
+        
+        return true;
     }
 }
