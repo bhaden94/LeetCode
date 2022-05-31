025829214d934a27e1a0d9fb7c6ac74d407bebdc
Brady Haden <59949412+bhaden94@users.noreply.github.com>
2022-05-31 09:52:32 -0400

    Time: 1 ms, Memory: 42.1 MB - LeetHub

--- binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -13,26 +13,46 @@
  *     }
  * }
  */
-class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        if (root == null) return new ArrayList<Integer>();
-        Stack<TreeNode> stack = new Stack<>();
-        List<Integer> tree = new ArrayList<>();
+// class Solution {
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         if (root == null) return new ArrayList<Integer>();
+//         Stack<TreeNode> stack = new Stack<>();
+//         List<Integer> tree = new ArrayList<>();
         
-        stack.push(root);
+//         stack.push(root);
         
-        while (!stack.empty()) {
-            TreeNode curr = stack.pop();
-            tree.add(curr.val);
+//         while (!stack.empty()) {
+//             TreeNode curr = stack.pop();
+//             tree.add(curr.val);
             
-            if (curr.right != null) {
-                stack.push(curr.right);
-            }
-            if (curr.left != null) {
-                stack.push(curr.left);
-            }
-        }
+//             if (curr.right != null) {
+//                 stack.push(curr.right);
+//             }
+//             if (curr.left != null) {
+//                 stack.push(curr.left);
+//             }
+//         }
         
-        return tree;
+//         return tree;
+//     }
+// }
+
+// recursive
+class Solution {
+    
+    private void helper(List<Integer> list, TreeNode node) {
+        if (node == null) return;
+        list.add(node.val);
+        helper(list, node.left);
+        helper(list, node.right);
+    }
+    
+    public List<Integer> preorderTraversal(TreeNode root) { 
+        List<Integer> list = new ArrayList<>();
+        if (root == null) { return list; }
+        
+        helper(list, root);
+        
+        return list;
     }
 }
