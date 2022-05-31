2fee511c771ea4b254628625517d1cf6505c210e
Brady Haden <59949412+bhaden94@users.noreply.github.com>
2022-05-31 09:56:50 -0400

    Time: 0 ms, Memory: 42.1 MB - LeetHub

--- binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -13,34 +13,54 @@
  *     }
  * }
  */
+// iterative
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         Stack<TreeNode> stack = new Stack<>();
+//         List<Integer> list = new ArrayList<>();
+//         if (root == null) return list;
+        
+//         stack.push(root);
+        
+//         // store in pre-order
+//         while (!stack.empty()) {
+//             TreeNode curr = stack.pop();
+//             list.add(curr.val);
+
+//             if (curr.left != null) {
+//                 stack.push(curr.left);
+//             }
+//             if (curr.right != null) {
+//                 stack.push(curr.right);
+//             }
+//         }
+        
+//         // reverse list
+//         int left = 0, right = list.size() - 1;
+//         while (left < right) {
+//             int temp = list.get(left);
+//             list.set(left++, list.get(right));
+//             list.set(right--, temp);
+//         }
+        
+//         return list;
+//     }
+// }
+
+// recursive
 class Solution {
+    private void helper(List<Integer> list, TreeNode node) {
+        if (node == null) return;
+        helper(list, node.left);
+        helper(list, node.right);
+        list.add(node.val);
+    }
+    
     public List<Integer> postorderTraversal(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<>();
         List<Integer> list = new ArrayList<>();
         if (root == null) return list;
         
-        stack.push(root);
-        
-        // store in pre-order
-        while (!stack.empty()) {
-            TreeNode curr = stack.pop();
-            list.add(curr.val);
-
-            if (curr.left != null) {
-                stack.push(curr.left);
-            }
-            if (curr.right != null) {
-                stack.push(curr.right);
-            }
-        }
-        
-        // reverse list
-        int left = 0, right = list.size() - 1;
-        while (left < right) {
-            int temp = list.get(left);
-            list.set(left++, list.get(right));
-            list.set(right--, temp);
-        }
+        helper(list, root);
         
         return list;
     }
