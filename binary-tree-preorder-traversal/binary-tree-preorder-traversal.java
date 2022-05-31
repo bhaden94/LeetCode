2f4673fd92c5729fbcf727620b4a270ad0d75976
Brady Haden <59949412+bhaden94@users.noreply.github.com>
2022-05-31 09:52:48 -0400

    Time: 0 ms, Memory: 42 MB - LeetHub

--- binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+// iterative
 // class Solution {
 //     public List<Integer> preorderTraversal(TreeNode root) {
 //         if (root == null) return new ArrayList<Integer>();
