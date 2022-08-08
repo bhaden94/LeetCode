d4f714e17e7fd5cd724fd1cc18259cd56d53a7ed
Brady Haden <59949412+bhaden94@users.noreply.github.com>
2022-08-08 11:24:48 -0400

    Time: 707 ms, Memory: 118.3 MB - LeetHub

--- open-the-lock/open-the-lock.java
+++ open-the-lock/open-the-lock.java
@@ -2,47 +2,52 @@ class Solution {
     public int openLock(String[] deadends, String target) {
         Set<String> visited = new HashSet<>();
         Set<String> deads = new HashSet<>(Arrays.asList(deadends));
-        Queue<String> q = new LinkedList<>();
+        Queue<String> combinations = new LinkedList<>();
         int turns = 0;
-        q.offer("0000");
+        
+        combinations.offer("0000");
         visited.add("0000");
         
-        while (!q.isEmpty()) {
-            int size = q.size();
+        while (!combinations.isEmpty()) {
+            int size = combinations.size();
+            
             while (size > 0) {
-                String currentPosition = q.poll();
-                if (deads.contains(currentPosition)) {
+                String currentCombination = combinations.poll();
+                if (deads.contains(currentCombination)) {
                     size--;
                     continue;
                 }
-
-                if (currentPosition.equals(target)) {
+                
+                if (currentCombination.equals(target)) {
                     return turns;
                 }
-                StringBuilder sb = new StringBuilder(currentPosition);
-                // calculate all positions and add to queue
+                
+                // try all combinations
                 for (int i=0; i<4; i++) {
-                    char curr = sb.charAt(i);
-                    String sbIncrement = sb.substring(0, i) +
-                        (curr == '9' ? 0 : curr - '0' + 1) + sb.substring(i + 1);
-
-                    String sbDecrement = sb.substring(0, i) +
-                        (curr == '0' ? 9 : curr - '0' - 1) + sb.substring(i + 1);
-
-                    if (!visited.contains(sbIncrement) && !deads.contains(sbIncrement)) {
-                        q.offer(sbIncrement);
-                        visited.add(sbIncrement);
+                    char currentWheel = currentCombination.charAt(i);
+                    int wheelPlusOne = (currentWheel == '9') ? 0 : currentWheel - '0' + 1;
+                    int wheelMinusOne = currentWheel == '0' ? 9 : currentWheel - '0' - 1;
+                    
+                    String combinationUp = currentCombination.substring(0,i) +
+                        wheelPlusOne + currentCombination.substring(i+1);
+                    
+                    String combinationDown = currentCombination.substring(0,i) +
+                        wheelMinusOne + currentCombination.substring(i+1);
+                    
+                    if (!visited.contains(combinationUp) && !deads.contains(combinationUp)) {
+                        combinations.offer(combinationUp);
+                        visited.add(combinationUp);
                     }
                     
-                    if (!visited.contains(sbDecrement) && !deads.contains(sbDecrement)) {
-                        q.offer(sbDecrement);
-                        visited.add(sbDecrement);
+                    if (!visited.contains(combinationDown) && !deads.contains(combinationDown)) {
+                        combinations.offer(combinationDown);
+                        visited.add(combinationDown);
                     }
                 }
+                
                 size--;
             }
-            
-            turns++;
+            turns++;    
         }
         
         return -1;
