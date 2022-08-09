367aae8cde9c203313dc55547f525e47fb1b38d4
Brady Haden <59949412+bhaden94@users.noreply.github.com>
2022-08-09 11:06:28 -0400

    Time: 14 ms (8.05%), Space: 55 MB (7.31%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -0,0 +1,57 @@
+class Solution {
+    public void solve(char[][] board) {
+        Queue<List<Integer>> boardSpots = new LinkedList<>();
+        Set<List<Integer>> shouldNotFlip = new HashSet<>();
+        
+        // find spots we don't want to flip
+        for (int row=0; row<board.length; row++) {
+            for (int col=0; col<board[row].length; col++) {
+                if ((board[row][col] == 'O') &&
+                    ((row == 0 || col == 0) ||
+                    (row == board.length - 1 || col == board[row].length - 1))) {
+                    // BFS on all connected nodes
+                    boardSpots.offer(List.of(row, col));
+                    shouldNotFlip.add(List.of(row, col));
+                    
+                    while (!boardSpots.isEmpty()) {
+                        List<Integer> currentSpot = boardSpots.poll();
+                        int x = currentSpot.get(0);
+                        int y = currentSpot.get(1);
+                        
+                        // check surrounding nodes
+                        if (x-1 >= 0 && board[x-1][y] == 'O' && !shouldNotFlip.contains(List.of(x-1, y))) {
+                            List<Integer> nextO = List.of(x-1, y);
+                            shouldNotFlip.add(nextO);
+                            boardSpots.offer(nextO);
+                        }
+                        if (x+1 < board.length && board[x+1][y] == 'O' && !shouldNotFlip.contains(List.of(x+1, y))) {
+                            List<Integer> nextO = List.of(x+1, y);
+                            shouldNotFlip.add(nextO);
+                            boardSpots.offer(nextO);
+                        }
+                        if (y-1 >= 0 && board[x][y-1] == 'O' && !shouldNotFlip.contains(List.of(x, y-1))) {
+                            List<Integer> nextO = List.of(x, y-1);
+                            shouldNotFlip.add(nextO);
+                            boardSpots.offer(nextO);
+                        }
+                        if (y+1 < board[x].length && board[x][y+1] == 'O' && !shouldNotFlip.contains(List.of(x, y+1))) {
+                            List<Integer> nextO = List.of(x, y+1);
+                            shouldNotFlip.add(nextO);
+                            boardSpots.offer(nextO);
+                        }
+                    }
+                }
+            }
+        }
+        
+        // flip all other 'O's
+        for (int row=0; row<board.length; row++) {
+            for (int col=0; col<board[row].length; col++) {
+                if (board[row][col] == 'O' && !shouldNotFlip.contains(List.of(row, col))) {
+                    board[row][col] = 'X';
+                }
+            }
+        }
+        
+    }
+}
