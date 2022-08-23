841eb3d40faff11f2620c5bada42c0534f03c7be
Brady Haden <59949412+bhaden94@users.noreply.github.com>
2022-08-23 10:44:48 -0400

    Time: 22 ms, Memory: 56.5 MB - LeetHub

--- number-of-islands/number-of-islands.java
+++ number-of-islands/number-of-islands.java
@@ -1,7 +1,49 @@
 class Solution {
+    // BFS
+//     public int numIslands(char[][] grid) {
+//         // [row,col]
+//         Queue<Integer[]> q = new LinkedList<>();
+//         int islands = 0;
+        
+//         for (int row = 0; row<grid.length; row++) {
+//             for (int col = 0; col<grid[row].length; col++) {
+//                 if (grid[row][col] == '0') continue;
+                
+//                 // if 1, find all surrounding ones
+//                 q.offer(new Integer[]{row,col});
+//                 grid[row][col] = '0';
+//                 islands++;
+//                 while (!q.isEmpty()) {
+//                     Integer[] position = q.poll();
+//                     int x = position[0];
+//                     int y = position[1];
+//                     // check surrounding
+//                     if (x+1 < grid.length && grid[x+1][y] == '1') {
+//                         grid[x+1][y] = '0';
+//                         q.offer(new Integer[]{x+1,y});
+//                     }
+//                     if (x-1 >= 0 && grid[x-1][y] == '1') {
+//                         grid[x-1][y] = '0';
+//                         q.offer(new Integer[]{x-1,y});
+//                     }
+//                     if (y+1 < grid[x].length && grid[x][y+1] == '1') {
+//                         grid[x][y+1] = '0';
+//                         q.offer(new Integer[]{x,y+1});
+//                     }
+//                     if (y-1 >= 0 && grid[x][y-1] == '1') {
+//                         grid[x][y-1] = '0';
+//                         q.offer(new Integer[]{x,y-1});
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return islands;
+//     }
+    
+    // DFS
     public int numIslands(char[][] grid) {
-        // [row,col]
-        Queue<Integer[]> q = new LinkedList<>();
+        Stack<Integer[]> s = new Stack<>();
         int islands = 0;
         
         for (int row = 0; row<grid.length; row++) {
@@ -9,29 +51,29 @@ class Solution {
                 if (grid[row][col] == '0') continue;
                 
                 // if 1, find all surrounding ones
-                q.offer(new Integer[]{row,col});
+                s.add(new Integer[]{row,col});
                 grid[row][col] = '0';
                 islands++;
-                while (!q.isEmpty()) {
-                    Integer[] position = q.poll();
+                while (!s.empty()) {
+                    Integer[] position = s.pop();
                     int x = position[0];
                     int y = position[1];
                     // check surrounding
                     if (x+1 < grid.length && grid[x+1][y] == '1') {
                         grid[x+1][y] = '0';
-                        q.offer(new Integer[]{x+1,y});
+                        s.add(new Integer[]{x+1,y});
                     }
                     if (x-1 >= 0 && grid[x-1][y] == '1') {
                         grid[x-1][y] = '0';
-                        q.offer(new Integer[]{x-1,y});
+                        s.add(new Integer[]{x-1,y});
                     }
                     if (y+1 < grid[x].length && grid[x][y+1] == '1') {
                         grid[x][y+1] = '0';
-                        q.offer(new Integer[]{x,y+1});
+                        s.add(new Integer[]{x,y+1});
                     }
                     if (y-1 >= 0 && grid[x][y-1] == '1') {
                         grid[x][y-1] = '0';
-                        q.offer(new Integer[]{x,y-1});
+                        s.add(new Integer[]{x,y-1});
                     }
                 }
             }
